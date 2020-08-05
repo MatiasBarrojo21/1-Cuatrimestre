6209375aedaa0675db30c2795d86ebfbeadddbb5
Juan Ignacio Suarez <nachomam123@gmail.com>
2020-08-04 21:54:32 -0300

    Update MAYMEN.cpp

--- MAYMEN.cpp
+++ MAYMEN.cpp
@@ -2,50 +2,45 @@
 #include <stdlib.h>
 #include <time.h>
 
-int NumeroGenerado(int Num);
-void Inicio(int N, int Y, int Puntaje );
+int NumeroGenerado();
+void Inicio(int N, int Y, int Puntaje);
+void rango(int &mayor, int &menor, int NumOrig, int Num_Ingresado);
+
 
 main(){
-	int n, P, Punt=10, y;
-		y= NumeroGenerado(P);
-		printf("\n MAYMEN");
+	int n, Punt=10, y;
+		y= NumeroGenerado();
+		printf("\n MAYMEN %d", 537);
 		printf("\n ======");
-		Inicio(n, y, Punt);
-
+		Inicio(n, 537, Punt);
 }
 
-int NumeroGenerado(int Num){
+int NumeroGenerado(){
 	srand(time(NULL));
-	Num= 1 + rand()%(999+1 - 1);
+	return 1 + rand()%(999+1 - 1);
 }
 void Inicio(int N, int Y, int Puntaje ){
-	 for (int X=1; X<=10; X++){
+	
+	int numMay=999, numMen=1;
+	
+	for (int X=1; X<=10; X++){
 			printf("\n Intento %d: ",X);
 			scanf("%d", &N);
 
 			if(N>=1 && N<=999){
+				rango(numMay, numMen, Y, N);
 				if (N==Y){
 					X=10;
 				}
 					if (N>Y){
-						if (Y-50<1){
-							printf("\n El numero secreto es menor y se encuentra entre %d y 1\n", N-1);
-				}else{
-					printf("\n El n�mero secreto es menor y se encuentra entre %d y %d\n", N-1,Y-50);
-				}
-
-						Puntaje=Puntaje-1;
+						printf("\n El numero secreto es menor y se encuentra entre %d y %d\n", numMen, numMay);
+						Puntaje--;
 					}
-						if (N<Y){
-								if (Y+50>999){
-						 			printf("\n El numero secreto es mayor y se encuentra entre %d y 999\n", N+1);
-						}else{
-							printf("\n El numero secreto es mayor y se encuentra entre %d y %d\n", N+1,Y+50);
-						}
-
-						Puntaje=Puntaje-1;
+					if (N<Y){
+						printf("\n El numero secreto es mayor y se encuentra entre %d y %d\n", numMen, numMay);
+						Puntaje--;
 					}
-		}
+			}
 			else{
 				printf("\nTiene que ingresar un numero entre 1 y 999, intente nuevamente\n");
 				X--;
@@ -58,4 +53,14 @@ void Inicio(int N, int Y, int Puntaje ){
 		printf("\n Perdiste! Puntaje Obtenido %d", Puntaje);
 		printf("\n El nuemero a adivinar era %d", Y);
 	}
+}
+
+void rango(int &mayor, int &menor, int NumOrig, int Num_Ingresado){
+	if(Num_Ingresado<NumOrig and Num_Ingresado>menor){
+		menor=Num_Ingresado;
+	}
+	if(Num_Ingresado>NumOrig and Num_Ingresado<mayor){
+		mayor=Num_Ingresado;
+	}
+	
 }
