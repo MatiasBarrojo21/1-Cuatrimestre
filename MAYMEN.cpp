75c8ee8ffd5d7d34b3268d9356581f048a310feb
Matias Barrojo <68956035+Matias2101@users.noreply.github.com>
2020-08-17 18:02:59 -0300

    Se le modificaron unas cosas
    Se le agregaron 2 mensajes en la funcion main mostrando el primer mensaje "Saliendo del juego..." y el otro mensaje "Adios...." 
    Tambien se le borraron librerias innecesarias ya que esas librerias estan implmentadas en la libreria Owntools.h

--- MAYMEN.cpp
+++ MAYMEN.cpp
@@ -1,17 +1,12 @@
-#include <stdio.h>
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-#include "main.cpp"
+#include "Owntools.h"
 
+//Fucniones
 int NumeroGenerado();
 void Inicio(int N, int Y, int Puntaje);
 void rango(int &mayor, int &menor, int NumOrig, int Num_Ingresado);
-void titleMM();
-void barracarga();;
 
-
-maymen(){
+main(){
+	system("mode 120, 35");
 	int n, Punt=10, y;
 	bool op=true;
 	int verificar;
@@ -20,145 +15,79 @@ maymen(){
 		Inicio(n, y, Punt);
 		gotoxy(5, 18); printf("\n%c Quieres volver a jugar? (Para SI ingrese '1', para NO ingrese '2'): ", 175);
 		scanf("%d", &verificar);
-		printf("\n");
 				if(verificar==2){
 					op=false;
-					system("cls");
-					main();
+					printf("\nSaliendo del juego......\n");
+					printf("\nAdios....\n");
 				}
 				else{
 					system("cls");
 				}
 	}
-	system("cls");	
 }
 
 int NumeroGenerado(){
 	srand(time(NULL)); 
 	return 1 + rand()%(999+1 - 1);
 }
-void Inicio(int N, int Y, int Puntaje ){
-	
-	int numMay=999, numMen=1;
-	titleMM();
-	for (int X=1; X<=10; X++){
-			printf("\n\n\t\t\t============================== Intento %d/10 ============================== \n\n\n\t\t\t",X);
-			printf("EL numero se encuentra entre %d y %d\n\t\t\t", numMen, numMay);
-			printf("Ingrese el numero: ");
-			scanf("%d", &N);
 
-			if(N>=1 && N<=999){
-				rango(numMay, numMen, Y, N);
-				if (N==Y){
-					X=10;
+void Inicio(int N, int Y, int Puntaje ){
+	int numMay=999, numMen=1;
+	int ejeY=15, Contador=1;
+	textcolor(WHITE);
+	gotoxy(27, 10); printf("===========================================================================");
+	textcolor(DARKGRAY);
+	gotoxy(34, 12); printf("%cTiene que adivinar un numero de 3 cifras que esta entre 1 y 999%c", 175, 174);
+		for (int X=1; X<=10; X++){
+			TituloMYM();
+			textcolor(LIGHTRED);
+			gotoxy(57, ejeY); printf("INTENTO %d/10\n", X);
+			textcolor(WHITE);
+			printf("\n\t\t\t%cIngrese el numero: ", 175);
+			scanf("%d", &N);
+				if(N>=1 && N<=999){
+					rango(numMay, numMen, Y, N);
+					textcolor(MAGENTA);
+						if (N==Y){
+							X=10;
+							system("cls");
+						}
+							if (N>Y){
+								printf("\n\t\t\t\tEl numero secreto es menor y se encuentra entre %d y %d\n", numMen, numMay);
+								Puntaje--;
+							}
+								if (N<Y){
+									printf("\n\t\t\t\tEl numero secreto es mayor y se encuentra entre %d y %d\n", numMen, numMay);
+									Puntaje--;	
+								}
 				}
-					if (N>Y){
-						printf("\t\t\tEl numero secreto es menor y se encuentra entre %d y %d\n\n\n\t\t\t", numMen, numMay);
-						Puntaje--;
-						crearLineas('=', 71);
+				else{
+					printf("\n\t\t\t\tTiene que ingresar un numero entre 1 y 999, intente nuevamente\n");
+					X--;
+				}
+			ejeY=ejeY+6;
+			Contador++;
+					if(Contador==4){
+						textcolor(WHITE);
+						printf("\n");
+						system("pause");
+						system("cls");
+						gotoxy(27, 10); printf("===========================================================================");
+						textcolor(DARKGRAY);
+						gotoxy(43, 12); printf("%cEl numero se encuentra entre %d y %d%c", 175, numMen, numMay, 174);
+						ejeY=15;
+						Contador=1;
 					}
-					if (N<Y){
-						printf("\t\t\tEl numero secreto es mayor y se encuentra entre %d y %d\n\n\n\t\t\t", numMen, numMay);
-						Puntaje--;
-						crearLineas('=', 71);
-					}
-			}
-			else{
-				printf("\t\t\tTiene que ingresar un numero entre 1 y 999, intente nuevamente\n\n\n\t\t\t");
-				X--;
-				crearLineas('=', 71);
-			}
-			if(X<10){
-				barracarga();
-			}
-			system("cls");
-			titleMM();
 		}
-		
-	if(Puntaje>=1){
-		textcolor(GREEN);
-		for(int i=43; i<80; i++){
-			gotoxy(i, 9); printf("%c", 205); //63 28
-			gotoxy(i, 15); printf("%c", 205);
-		}
-		
-			for(int i=9; i<16; i++){
-				gotoxy(43, i); printf("%c", 186);
-				gotoxy(79, i); printf("%c", 186);
-			}
-		
-			gotoxy(43, 9); printf("%c", 201);
-			gotoxy(43, 15); printf("%c", 200);
-			gotoxy(79, 9); printf("%c", 187);
-			gotoxy(79, 15); printf("%c", 188);
-			gotoxy(46, 12); printf("Acertaste! Puntaje obtenido: %d", Puntaje);
-			textcolor(WHITE);
-			printf("\n\n\n\n\n\n\n");
-	}else{
-		textcolor(LIGHTRED);
-		for(int i=45; i<79; i++){
-			gotoxy(i, 9); printf("%c", 205); //63 28
-			gotoxy(i, 16); printf("%c", 205);
-		}
-		
-			for(int i=9; i<17; i++){
-				gotoxy(45, i); printf("%c", 186);
-				gotoxy(78, i); printf("%c", 186);
-			}
-		
-			gotoxy(45, 9); printf("%c", 201);
-			gotoxy(45, 16); printf("%c", 200);
-			gotoxy(78, 9); printf("%c", 187);
-			gotoxy(78, 16); printf("%c", 188);
-		
-			gotoxy(48, 12); printf("Perdiste! Puntaje Obtenido %d", Puntaje);
-			gotoxy(48, 13); printf("El nuemero a adivinar era: %d", Y);
-			textcolor(WHITE);
-			printf("\n\n\n\n\n\n\n");
-	}
+	system("cls");
+	MarcoResultadoMYM(Puntaje, Y);
 }
 
 void rango(int &mayor, int &menor, int NumOrig, int Num_Ingresado){
 	if(Num_Ingresado<NumOrig and Num_Ingresado>menor){
 		menor=Num_Ingresado;
 	}
-	if(Num_Ingresado>NumOrig and Num_Ingresado<mayor){
-		mayor=Num_Ingresado;
-	}
-	
-}
-
-void barracarga(){
-	textcolor(YELLOW);
-	gotoxy(25, 18);
-	
-	for(int i=0; i<4;i++){
-		Sleep(400);
-		crearLineas(219, 13);
-		cout<<"      ";
-	}
-	textcolor(WHITE);
-	cout<<"\n";
-}
-
-void titleMM(){
-	textcolor(YELLOW);
-	gotoxy(11, 5);crearLineas(205, 42);cout<<">";
-	gotoxy(72, 5);cout<<"<";crearLineas(205, 42);
-	textcolor(LIGHTGREEN );
-			gotoxy(60, 5);printf("MAYMEN");
-		for(int i=54; i<71; i++){
-			gotoxy(i, 4); printf("%c", 205);
-			gotoxy(i, 6); printf("%c", 205);
+		if(Num_Ingresado>NumOrig and Num_Ingresado<mayor){
+			mayor=Num_Ingresado;
 		}
-			for(int i=4; i<7; i++){
-			gotoxy(54, i); printf("%c", 186);
-			gotoxy(71, i); printf("%c", 186);
-		}
-		
-		gotoxy(54, 4); printf("%c", 201);
-		gotoxy(54, 6); printf("%c", 200);
-		gotoxy(71, 4); printf("%c", 187);
-		gotoxy(71, 6); printf("%c", 188);
-		textcolor(WHITE);
 }
